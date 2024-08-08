67bd29ec9c79272e6871abfeea03745f5d8c523c
Umar295c <itxcoding295c@gmail.com>
2024-08-08 13:19:56 +0500

    new files added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import express from "express";
+const app = express();
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB().then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server listening on ${process.env.PORT}`);
+  });
+}).catch((err) => {
+  console.log(`Connection error: ${err}`);
+  
+})
 
 // const app=express();
 // ;(async()=>{
